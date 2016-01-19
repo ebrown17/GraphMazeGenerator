274508bd9c4226a52778104970a91e890c276849
Evan Brown <ebrown7871@gmail.com>
2016-01-18 17:38:32 -0800

    updated

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,4 @@
 import java.util.ArrayList;
-import java.util.Random;
-
 import graphUtil.GraphNode;
 import maze.RecursiveMaze;
 import solvers.AStar;
@@ -8,43 +6,18 @@ import solvers.AStar;
 public class Main {
 	
 	static int row = 15;
-	static int column = 90;
+	static int column = 30;
 	static ArrayList<GraphNode> maze;
 	
 	public static void main(String args[]){		
 		
-		maze = new AStar(new RecursiveMaze(row,column).getMaze(),false).getMaze();		
+		maze = new AStar(new RecursiveMaze(row,column).getMaze()).getMaze();		
 		printLevel(maze);
-		maze = new AStar(new RecursiveMaze(row,column).getMaze(),false).getMaze();		
+		maze = new AStar(new RecursiveMaze(row,column).getMaze()).getMaze();		
 		printLevel(maze);
-		maze = new AStar(new RecursiveMaze(row,column).getMaze(),false).getMaze();		
+		maze = new AStar(new RecursiveMaze(row,column).getMaze()).getMaze();		
 		printLevel(maze);
-		
-		/*nodes = GeneratorUtils.generateNodeList(row, column);		
-		
-		GeneratorUtils.setEdgeNodes(row,column,nodes);
-		
-		//System.out.println((int)(Math.random()*nodes.size()));
-		GeneratorUtils.generateRecursive(nodes,row,column);
-		Node start=null,end=null;
-		for(Node node: nodes){
-			if(node.getState().equals("1"))node.cost=1000;
-			if(node.getState().equals("S"))start=node;
-			if(node.getState().equals("E"))end=node;
-			
-		}
-		long startTime = System.nanoTime();
-		
-		GeneratorUtils.AStarSearch(start,end);		
-		//GeneratorUtils.resetPriority(nodes);
-	//	GeneratorUtils.AStarSearchFast(start,end);
-		
-		long endTime = System.nanoTime();		
-		long totalTime = (endTime-startTime)/1000;
-		System.out.println((totalTime) + " ms to solve");
-		
-		
-		printLevel();*/
+	
 	}
 	
 	public static void printLevel(ArrayList<GraphNode> maze){
@@ -53,10 +26,8 @@ public class Main {
 			System.out.print(node.getState() + "");
 			if(count % column == 0) System.out.println("");
 			count++;
-		}
-	
+		}	
 	}
-
 }
 
 
