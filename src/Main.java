c4467c7ddcb5f4b19806d1117be3a576e660b5db
Evan Brown <ebrown7871@gmail.com>
2016-02-02 23:03:12 -0800

    updated and removed old code

--- src/Main.java
+++ src/Main.java
@@ -1,47 +1,69 @@
+import java.awt.Dimension;
 import java.util.ArrayList;
 
+import javax.swing.JFrame;
 import javax.swing.SwingUtilities;
 
-import graphUtil.GraphNode;
+import graphUtil.Vector2d;
+import graphUtil.nodes.GridNode;
 import graphics.Base;
-import maze.Maze;
-import maze.RecursiveMaze;
+import map.generation.mazes.RecursiveMaze;
 import solvers.AStar;
 
 public class Main {
 	
-	private static final int TILESIZE = 2;
+	private static final int TILESIZE = 5;
 	private static final int WIDTH = 800;
 	private static final int HEIGHT = 800;
 	
 	static int row = WIDTH/TILESIZE;
-	static int column = HEIGHT/TILESIZE;
-	static ArrayList<GraphNode> maze;
-	static Maze test;
-	static AStar path;
+	static int column = HEIGHT/TILESIZE;	
+	static RecursiveMaze test;
+	static ArrayList<Vector2d> path;
+	static Base base;
 	public static void main(String args[]){
 		
-		 test = new RecursiveMaze(row,column);
-		 path = new AStar(test.getNodeList());
+		/*while(true){
+			path =null;*/
+			while(null == path){ 
+				test = new RecursiveMaze(row,column); 
+				path = AStar.aStarSearch(test.getStart(),test.getEnd());		 
+			}
 		 
-		//maze = new AStar(new RecursiveMaze(row,column).getNodeList()).getMaze();		
-		//printLevel(maze);		
-		
 		 SwingUtilities.invokeLater(new Runnable() {
 				public void run() {
-					Base base = new Base(TILESIZE,WIDTH,HEIGHT,test.getNodeList());
-					base = new Base(TILESIZE,WIDTH,HEIGHT,path.getPath());
+					base = new Base(TILESIZE,WIDTH,HEIGHT,test);
+					
+					base.pack();               
+					base.getContentPane().setPreferredSize(new Dimension(WIDTH,HEIGHT));
+					base.setTitle("Practice");
+					base.pack();        
+					base.setLocationRelativeTo(null);
+					base.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+					base.setResizable(true);
+					base.setVisible(true);
+					base.addPath(path);
+					//base.addPath(path.getPath());
+					//base = new Base(TILESIZE,WIDTH,HEIGHT,path.getPath());
 					
 				}
-			});	        
-	    
-	
+			});	
+		 
+		
+		/* try {
+			Thread.sleep(2000);
+			base.dispose();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		}*/
 	}
 	
-	public static void printLevel(ArrayList<GraphNode> maze){
+	public static void printLevel(ArrayList<GridNode> maze){
 		int count =1;
-		for(GraphNode node: maze){			
-			System.out.print(node.getState() + "");
+		for(GridNode node: maze){			
+			System.out.print(node.tile.chr + "");
 			if(count % row == 0) System.out.println("");
 			count++;
 		}	
