b458ed075893f83a499b170165d30d3d84185019
Evan Brown <ebrown7871@gmail.com>
2016-02-02 23:03:44 -0800

    added support for redrawing the maze

--- src/graphics/Base.java
+++ src/graphics/Base.java
@@ -1,40 +1,92 @@
 package graphics;
 
-import java.awt.Dimension;
 import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.util.ArrayList;
+import java.util.Collections;
 
 import javax.swing.JFrame;
 
+import graphUtil.Vector2d;
+import graphUtil.nodes.GridNode;
+import map.TileType;
+import map.generation.mazes.RecursiveMaze;
+import solvers.AStar;
 
-import graphUtil.GraphNode;
-
-public class Base extends JFrame {	
+public class Base extends JFrame implements Runnable  {	
 	
-    public Base(int tileSize,int width, int height,ArrayList<GraphNode> maze) {
-
+	private Thread mainGame;
+	private int tileSize, width,height;
+	private Surface surface;
+	private Thread test;
+	RecursiveMaze level;
+	ArrayList<Vector2d> path;
+	
+    public Base(int tileSize,int width, int height,RecursiveMaze maze) {
+    	this.tileSize=tileSize;
+    	this.width=width;
+    	this.height=height;
+    	this.level = maze;
         initUI(tileSize,width,height,maze);
+       
     }
 
-    private void initUI(final int tileSize,int width, int height,ArrayList<GraphNode> maze) {
-    	Surface surface = new Surface(tileSize,width,height,maze);
+    private void initUI(final int tileSize,int width, int height,RecursiveMaze maze) {
+    	surface = new Surface(tileSize,width,height,maze.getNodeList());    	
     	
     	surface.addMouseMotionListener(new MouseAdapter() {
     		 public void mouseMoved(MouseEvent e) {    			 
     			 	setTitle("X : " + (e.getX()/tileSize) + " Y : " + e.getY()/tileSize);    		      
     		    }
+    		 
     	});
     	
-        add(surface);
-        
-        pack();               
-        getContentPane().setPreferredSize(new Dimension(width,height));
-        setTitle("Practice");
-        pack();        
-        setLocationRelativeTo(null);
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setResizable(true);
-        setVisible(true);
+        surface.addMouseListener(new MouseAdapter() {
+    		 public void mouseClicked(MouseEvent e) {    			 
+    			 surface.clearPath();    			 
+    			 Vector2d newEnd = new Vector2d(e.getX()/tileSize,e.getY()/tileSize);
+    			 Vector2d oldEnd = new Vector2d(level.end.postion);
+    			 level.end.tile=TileType.CLEAR;
+    			 for(GridNode node: level.getNodeList()){
+    				 if(node.postion.sameVector(newEnd)){
+    					 node.tile=TileType.END;
+    					 level.end=node;
+    					 
+    					 break;
+    				 }
+    			 }
+    			 surface.moveEnd(oldEnd, newEnd);
+    			 path = AStar.aStarSearch(level.getStart(),level.getEnd());
+    			 
+    			 surface.addPath(path);
+    		    }
+    	});
+    	
+    	add(surface);
+    	
     }
+    
+    public void addPath(ArrayList<Vector2d> path){
+    	this.path = path;
+    	Collections.reverse(path);
+    	test = new Thread(this,"Display");
+    	test.start();
+    }
+
+	@Override
+	public void run() {
+		try {
+			Thread.sleep(1000);
+			surface.addPath(path);
+			
+		} catch (Exception e1) {
+			// TODO Auto-generated catch block
+			e1.printStackTrace();
+		}
+		
+	}
+
+	
+		
+	
 }
--- src/graphics/Surface.java
+++ src/graphics/Surface.java
@@ -7,45 +7,43 @@ import java.util.ArrayList;
 
 import javax.swing.JPanel;
 
-import graphUtil.GraphNode;
+import graphUtil.Vector2d;
+import graphUtil.nodes.GridNode;
 
 public class Surface extends JPanel {
 	
 	private int tileSize,width, height;
-	private ArrayList<GraphNode> maze;
+	private ArrayList<GridNode> maze;
+	private ArrayList<Vector2d> path = new  ArrayList<Vector2d>();
 	
-	public Surface(int tileSize,int width, int height,ArrayList<GraphNode> maze){
+	public Surface(int tileSize,int width, int height,ArrayList<GridNode> maze){
 		
 		this.tileSize=tileSize;
 		this.width=width;
 		this.height=height;
 		this.maze = maze;
-		
+		//System.out.println(maze.size());
 	}
 	
 	
 	private void doDrawing(Graphics g) {
 
-        Graphics2D g2d = (Graphics2D) g;
+        Graphics2D g2d = (Graphics2D) g;      
         
-        int wSpacer = (width%tileSize)/2;
-        int hSpacer = (height%tileSize)/2;
-        
-       
-        
-        for(GraphNode node : maze){
+        for(GridNode node : maze){
         	
-        	if(node.getState().equals("S")){
-				g2d.setColor(Color.GREEN);
-    			g2d.fillRect((node.x*tileSize), (node.y*tileSize),tileSize,tileSize);
-    			g2d.setColor(Color.black);
-    			g2d.drawRect(node.x*tileSize, node.y*tileSize, tileSize, tileSize);
-    			
-			}
+        	
+        	
+        	//if(node.getState().equals("S")){
+				g2d.setColor(node.tile.color);
+    			g2d.fillRect((node.postion.x*tileSize), (node.postion.y*tileSize),tileSize,tileSize);
+    			/*g2d.setColor(Color.black);
+    			g2d.drawRect(node.x*tileSize, node.y*tileSize, tileSize, tileSize);*/
+        }		
+			/*}
         	else if(node.getState().equals("X")){
         		g2d.setColor(Color.RED);
-    			g2d.fillRect((node.x*tileSize), (node.y*tileSize),tileSize,tileSize);
-    			System.out.println(node.x + " " + node.y);
+    			g2d.fillRect((node.x*tileSize), (node.y*tileSize),tileSize,tileSize);    			
     			g2d.setColor(Color.black);
     			g2d.drawRect(node.x*tileSize, node.y*tileSize, tileSize, tileSize);
         	}
@@ -60,7 +58,7 @@ public class Surface extends JPanel {
         	else if(node.getState().equals("o")){
         		g2d.setColor(Color.CYAN);
     			g2d.fillRect((node.x*tileSize), (node.y*tileSize),tileSize,tileSize);
-        	}
+        	}*/
         	
         //	g2d.setColor(Color.black);
 			//g2d.drawRect(node.x*tileSize, node.y*tileSize, tileSize, tileSize);
@@ -70,7 +68,7 @@ public class Surface extends JPanel {
 				g2d.setColor(Color.black);
     			g2d.fillRect((x*tileSize), (y*tileSize),tileSize,tileSize);
 			}*/
-        }
+        
         
       /*  for(int x=0;x<width/tileSize;x++){
 			for(int y=0;y<height/tileSize;y++){
@@ -84,10 +82,46 @@ public class Surface extends JPanel {
 				}				
 			}
 		}*/
-        
-      
+       
+        //g2d.dispose();
     }
 
+	public void addToPath(Vector2d point){
+		this.path.add(point);
+	}
+	
+	public void addPath(ArrayList<Vector2d> path){
+		this.path= path;
+		drawPath();
+	}
+	
+	private void drawPath(){
+		Graphics2D g2d =(Graphics2D) getGraphics();
+		 for(Vector2d point : path){
+	        	g2d.setColor(Color.cyan);
+				g2d.fillRect((point.x*tileSize), (point.y*tileSize),tileSize,tileSize);
+	        }
+	}
+	public void clearPath(){
+		clearPath(getGraphics());
+	}
+	public void moveEnd(Vector2d old, Vector2d newEnd){
+		Graphics2D g2d = (Graphics2D) getGraphics();
+		g2d.setColor(Color.white);
+		g2d.fillRect((old.x*tileSize), (old.y*tileSize),tileSize,tileSize);
+		
+		g2d.setColor(Color.RED);
+		g2d.fillRect((newEnd.x*tileSize), (newEnd.y*tileSize),tileSize,tileSize);
+	}
+	private void clearPath(Graphics g){
+		Graphics2D g2d = (Graphics2D) g;
+		for(Vector2d point : path){
+        	g2d.setColor(Color.white);
+			g2d.fillRect((point.x*tileSize), (point.y*tileSize),tileSize,tileSize);
+        }
+		
+		 g2d.dispose();
+	}
     @Override
     public void paintComponent(Graphics g) {
         super.paintComponent(g);       
